b908e7d8e74ad8fc3afbef6dfef08dc1c64a3a2b
kinghilal <46085303+kinghilal@users.noreply.github.com>
2020-01-09 22:08:34 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: '',    
+    TOKEN: '6sZLNHkg7c0MnEf8Y--_zvV9UzXEeiOC',    
     YT_API_KEY: 'api ', 
-    prefix: '',
-    devs: ['']
+    prefix: '+',
+    devs: ['539820878754480147']
 }
